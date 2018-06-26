6a84d243c295578668cfa77dc855b9fc4dfd82d4
Asia LapTop <asialaptop@Alizadees-MacBook-Pro.local>
2018-06-26 19:06:44 +0430

    new message

--- project1/ViewController.swift
+++ project1/ViewController.swift
@@ -10,14 +10,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var back: UIImageView!
+    @IBOutlet weak var title: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func makeStart(){
+        back.hidden = false
+        title.hidden= false
     }
 
 
